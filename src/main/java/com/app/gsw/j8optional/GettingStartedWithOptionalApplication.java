f6f709ae11ae0d9fa24f250864466cd443b0ec1b
HPxIO <hpx.io@keemail.me>
2021-10-16 20:32:11 +0530

    initial commit

---
+++ src/main/java/com/app/gsw/j8optional/GettingStartedWithOptionalApplication.java
@@ -0,0 +1,13 @@
+package com.app.gsw.j8optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedWithOptionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingStartedWithOptionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/gsw/j8optional/GettingStartedWithOptionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.gsw.j8optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingStartedWithOptionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
